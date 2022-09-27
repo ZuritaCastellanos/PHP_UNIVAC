22105b8e5f9cf0eaf03a92c62be1c47fa605b7ce
ZuritaCastellanos <82136638+ZuritaCastellanos@users.noreply.github.com>
2022-09-27 16:58:33 -0500

    Add files via upload

---
+++ ejercicio2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Lenguaje php</title>
+</head>
+<body>
+<?php
+
+echo"******************************************************<br>";
+echo "AUTOR: Fabian Castellanos<br>";
+echo "Creacion y uso de variables<br>";
+$vVar1 = 20150;
+$vVar2 = 343.915;
+$vVar3 = "Universidad del Valle de Cuernavaca, UNIVAC";
+echo $vVar1 .  "<br>" .  $vVar2  .  "<br>"  .  $vVar3;
+?>
+</body>
+</html>
